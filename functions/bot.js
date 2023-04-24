3fcdc24e3a1a2a3eb6c20f4d071b11a9a3d0b9f5
blackpiest <priest_okha@mail.ru>
2023-04-24 17:57:40 +0300

    Fix launch bot.

--- functions/bot.js
+++ functions/bot.js
@@ -7,51 +7,30 @@ function randomInteger(min, max) {
 }
 
 const resultArray = [
-  'Супермен',
-  'Флэш',
-  'Джокер',
-  'Железный-человек',
-  'Человек-паук',
-  'Халк',
-  'Тор',
-  'Чёрная пантера',
-  'Чёрная вдова',
-  'Человек-муравей',
-  'Бэтмен',
-  'Росомаха',
-  'Циклоп',
-  'Вижн',
-  'Алая ведьма',
-  'Соколиный глаз',
-  'Оса',
-  'Зелёный фонарь',
-  'Чёрный Адам',
-  'Шазам',
-  'пидор'
+  'Бот',
+  'Не бот'
 ]
 
-// bot.start(ctx => {
-//   console.log("Received /start command")
-//   try {
-//     return ctx.reply("Привет!")
-//   } catch (e) {
-//     console.error("error in start action:", e)
-//     return ctx.reply("Error occured")
-//   }
-// })
+bot.start(ctx => {
+  console.log("Received /start command")
+  try {
+    return ctx.reply("Привет!")
+  } catch (e) {
+    console.error("error in start action:", e)
+    return ctx.reply("Error occured")
+  }
+})
 
 bot.on('message', (ctx) => {
   const chatId = ctx.message.chat.id;
   if (ctx.message.text.includes('/cat')) {
-    ctx.telegram.sendMessage(chatId, `${ctx.message.from.first_name}, сегодня котиков не будет. Бот уехал в Балашиху.`);
+    ctx.telegram.sendMessage(chatId, `${ctx.message.from.first_name}, сегодня котиков не будет. Бот расширяет базу.`);
   }
 
   if (ctx.message.text.includes('/who')) {
     ctx.telegram.sendMessage(chatId, `${ctx.message.from.first_name}, сегодня ты – ${resultArray[randomInteger(0, resultArray.length - 1)]}`);
   }
 });
-
-bot.launch();
 
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
 exports.handler = async event => {
