fd87f7779a4ad5f849ee1cbf06f2ef8aea8d0ed8
blackpiest <priest_okha@mail.ru>
2023-04-25 19:24:57 +0300

    Крупное обновление.

--- src/index.js
+++ src/index.js
@@ -6,13 +6,47 @@ require('dotenv').config();
 const token = process.env.T_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 const whoIAmVariants = require('./database/whoIAm/variants.json');
-const { start } = require('./functions/start');
 const moment = require('moment/moment');
+
+bot.onText(/\/today/, (msg) => {
+  const chatId = msg.chat.id;
+  const filePath = path.resolve(__dirname, `../src/database/whoIAm/chats/${chatId}.json`);
+  const currentDate = moment().startOf('day').valueOf();
+
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    };
+
+    const fileParse = JSON.parse(data);
+    let resultArray = [];
+    // console.log(data);
+    if (fileParse.find(item => item.date === currentDate)) {
+      resultArray = fileParse.find(item => item.date === currentDate).users.map(item => ({ name: item.user.first_name, username: item.user.username, result: item.result }));
+      console.log(resultArray)
+    }
+    // console.log('resultArray', fileParse.users);
+
+    let result = `📊 Статистика дня: \n\n`;
+
+    if (resultArray.length > 0) {
+      resultArray.forEach(item => {
+        result = result + `${item.name}(@${item.username}) – ${item.result}\n`;
+      });
+    } else {
+      result = result + `отсутсвует`
+    }
+
+    bot.sendMessage(chatId, result);
+  })
+});
 
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const filePath = path.resolve(__dirname, `../src/database/whoIAm/chats/${chatId}.json`);
-  const currentDate = moment().startOf('day').utc(true).unix();
+  const currentDate = moment().startOf('day').valueOf();
 
   const whoIAmTemplate = [
     {
@@ -28,7 +62,15 @@ bot.on('message', (msg) => {
         fs.writeFileSync(filePath, jsonString, { flag: 'w', encoding: 'utf8' });
         bot.sendMessage(chatId, 'Привет! Я готов работать');
       }
+      if (msg.text.includes('/who')) {
+        bot.sendMessage(chatId, 'Сначала необходимо ввести команду /start');
+
+      }
     } else {
+      if (msg.text.includes('/start')) {
+        bot.sendMessage(chatId, 'Лол, я уже запущен и готов к работе!');
+      }
+
       if (msg.text.includes('/who')) {
         const result = whoIAmVariants[randomInteger(0, whoIAmVariants.length - 1)];
         fs.readFile(filePath, 'utf8', (err, data) => {
@@ -37,70 +79,75 @@ bot.on('message', (msg) => {
             return;
           }
           const fileParse = JSON.parse(data);
+          let isNewRequest = false;
 
           for (let i = 0; i < fileParse.length; i++) {
-            if (fileParse[i].date === currentDate && fileParse[i].users?.find(user => user.id === msg.from.id)) {
-              const currentUserResult = fileParse[i].users.find(user => user.id === msg.from.id).result;
-              bot.sendMessage(chatId, `${msg.from.first_name}, напоминаю, сегодня ты – ${currentUserResult}!`);
+            if (fileParse[i].date === currentDate && fileParse[i].users?.find(item => item.user.id === msg.from.id)) {
+              const currentUserResult = fileParse[i].users.find(item => item.user.id === msg.from.id).result;
+              bot.sendMessage(chatId, `${msg.from.first_name}, напоминаю, сегодня ты – ${currentUserResult}`);
               break;
             }
-            if (fileParse[i].date === currentDate && !fileParse[i].users?.find(user => user.id === msg.from.id)) {
+            if (fileParse[i].date === currentDate && !fileParse[i].users?.find(item => item.user.id === msg.from.id)) {
               if (fileParse[i].users) {
-                fileParse[i].users.push({ id: msg.from.id, result });
+                fileParse[i].users.push({ user: msg.from, result });
               } else {
-                fileParse[i].users = [{ id: msg.from.id, result }];
+                fileParse[i].users = [{ user: msg.from, result }];
               }
               bot.sendMessage(chatId, `${msg.from.first_name}, сегодня ты – ${result}`);
+              isNewRequest = true;
               break;
             } else {
               fileParse.push({
                 date: currentDate,
                 users: [{
-                  id: msg.from.id,
+                  user: msg.from,
                   result: result
                 }],
               });
+              isNewRequest = true;
             }
           }
-
-          const newFile = JSON.stringify(fileParse);
-          fs.writeFile(filePath, newFile, error => console.log("Done"));
+          if (isNewRequest) {
+            const newFile = JSON.stringify(fileParse);
+            fs.writeFileSync(filePath, newFile);
+            console.log('Файл перезаписан!')
+          }
         });
       }
 
-      if (msg.text.includes('/botday')) {
-        fs.readFile(filePath, 'utf8', (err, data) => {
-          if (err) {
-            console.error(err);
-            return;
-          };
+      // if (msg.text.includes('/today')) {
+      //   fs.readFile(filePath, 'utf8', (err, data) => {
+      //     if (err) {
+      //       console.error(err);
+      //       return;
+      //     };
 
-          const fileParse = JSON.parse(data);
+      //     const fileParse = JSON.parse(data);
 
-          for (let i = 0; i < fileParse.length; i++) {
-            if (fileParse[i].date === currentDate && fileParse[i].users && fileParse[i].users.length > 0) {
-              const currentUsers = fileParse[i].users;
-              // const users = currentUsers.map(u => bot.chat.currentUsers);
+      //     for (let i = 0; i < fileParse.length; i++) {
+      //       if (fileParse[i].date === currentDate && fileParse[i].users && fileParse[i].users.length > 0) {
+      //         const currentUsers = fileParse[i].users;
+      //         // const users = currentUsers.map(u => bot.chat.currentUsers);
 
-              const requests = currentUsers.map(u => bot.getChatMember(chatId, u.id));
-              let resultString = '';
-              Promise.all(requests)
-                .then(response => {
-                  console.log('hello')
+      //         const requests = currentUsers.map(u => bot.getChatMember(chatId, u.id));
+      //         let resultString = '';
+      //         Promise.all(requests)
+      //           .then(response => {
+      //             console.log(response)
 
-                  for (let i = 0; i < response.length; i++) {
-                    for (let j = 0; j < currentUsers.length; i++) {
-                      if (response[i]?.user?.id === currentUsers[j].id) {
-                        resultString = `${resultString}\n${response[i]?.user?.first_name} – ${currentUsers[j].result}`;
-                      }
+      //             for (let i = 0; i < response.length; i++) {
+      //               for (let j = 0; j < currentUsers.length; i++) {
+      //                 if (response[i]?.user?.id === currentUsers[j].id) {
+      //                   resultString = `${resultString}\n${response[i]?.user?.first_name} – ${currentUsers[j].result}`;
+      //                 }
 
-                    }
-                  }
-                })
-            }
-          }
-        })
-      }
+      //               }
+      //             }
+      //           })
+      //       }
+      //     }
+      //   })
+      // }
     }
   });
 });
